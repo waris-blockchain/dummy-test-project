cf1b50f1902f5a732e6909fcece8d79279512ea9
Waris <khan312waris@gmail.com>
2024-09-30 07:30:44 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ function NavbarComponent() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">ss</Navbar.Brand>
+        <Navbar.Brand href="#home">Dummy Ticket Generator POC</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
